95b3d124707045a25c4195049b1817323cdf48fa
tudorrw <143077299+tudorrw@users.noreply.github.com>
2024-03-05 09:28:33 +0200

    Modifing some files in 2_semester

--- 2_semester/DSA/lab_2/PriorityQueue_sll/cmake-build-debug/cmake_install.cmake
+++ 2_semester/DSA/lab_2/PriorityQueue_sll/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/UBB/sem 2/DSA/labor 2/PriorityQueue sll
+# Install script for directory: /Users/tudor/UBB/2_semester/DSA/lab_2/PriorityQueue_sll
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/PriorityQueue_sll")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.3.2/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/UBB/sem 2/DSA/labor 2/PriorityQueue sll/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/tudor/UBB/2_semester/DSA/lab_2/PriorityQueue_sll/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
